142876a3f0af97b950615d6a81797b208241f473
nisehatakiti <nisehatakiti@gmail.com>
2026-09-15 10:17:36 +0900

    Bump AuthCore database version for sessions

--- src/Config.php
+++ src/Config.php
@@ -7,7 +7,7 @@ namespace AuthCore;
 final class Config
 {
     public const VERSION = AUTHCORE_VERSION;
-    public const DB_VERSION = '1.1.0';
+    public const DB_VERSION = '1.2.0';
     public const OPTION_DB_VERSION = 'authcore_db_version';
     public const OPTION_SETTINGS = 'authcore_settings';
 }
